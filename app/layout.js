f0031a212732203a98b59427fa01c1d35a91f400
eitank19 <142161766+eitank19@users.noreply.github.com>
2024-03-22 12:59:05 +0200

    Revert "Update layout.js (add GTM)"
    This reverts commit 8f20c4c198ea493f76777e15757c3e8b73a07e8f.

--- app/layout.js
+++ app/layout.js
@@ -1,38 +1,22 @@
 import { Rubik } from 'next/font/google';
-import Head from 'next/head';
 import './globals.css';
 import Header from './components/Header';
 
 const rubik = Rubik({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'ProActive | חדר הכושר של דרום הרמה',
+  title: 'ProActive',
   description:
-    'שמעתם נכון, אפילו ברמת הגולן הפריפאיטלית אפשר להתאמן כמו שצריך - עם מאמנים מקצועיים, מכשירים מכל הסוגים, מגוון חוגים ועוד מלא הפתעות',
+    'Elevate your fitness journey with Pro Active - where every workout becomes a step towards your best self. ',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NQXDVMKB');
-            `,
-          }}
-        />
-      </Head>
-      <html lang="he" dir="rtl">
-        <body className={rubik.className}>
-          <Header />
-          {children}
-        </body>
-      </html>
-    </>
+    <html lang="he" dir="rtl">
+      <body className={rubik.className}>
+        <Header />
+        {children}
+      </body>
+    </html>
   );
 }
