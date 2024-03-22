93712c5c911d12d2f4a78faadacc44b789333718
eitank19 <142161766+eitank19@users.noreply.github.com>
2024-03-22 13:26:09 +0200

    Revert "Update layout.js"
    This reverts commit 6e90e28c0f1dc66ad46825a91f2ccbc824910bcb.

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,19 @@
 import { Rubik } from 'next/font/google';
 import './globals.css';
 import Header from './components/Header';
-import GoogleTagManager from '@magicul/next-google-tag-manager';
 
 const rubik = Rubik({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'ProActive | חדר הכושר של דרום הרמה',
+  title: 'ProActive',
   description:
-    'שמעתם נכון, אפילו ברמת הגולן הפריפאיטלית אפשר להתאמן כמו שצריך - עם מאמנים מקצועיים, מכשירים מכל הסוגים, מגוון חוגים ועוד מלא הפתעות',
+    'Elevate your fitness journey with Pro Active - where every workout becomes a step towards your best self. ',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="he" dir="rtl">
       <body className={rubik.className}>
-      <GoogleTagManager id="GTM-NQXDVMKB" />
         <Header />
         {children}
       </body>
