e30278996042c139a2ef8d761ea6662819721baa
eitank19 <142161766+eitank19@users.noreply.github.com>
2024-03-23 21:04:01 +0200

    Revert "Update layout.js"
    This reverts commit 6e90e28c0f1dc66ad46825a91f2ccbc824910bcb.

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,16 @@
 import { Rubik } from 'next/font/google';
 import './globals.css';
 import Header from './components/Header';
-import { useClient } from 'next/client'; // Import useClient
 
 const rubik = Rubik({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'ProActive | חדר הכושר של דרום הרמה',
   description:
-    'שמעתם נכון, אפילו ברמת הגולן הפריפאיטלית אפשר להתאמן כמו שצריך - עם מאמנים מקצועיים, מכשירים מכל הסוגים, מגוון חוגים ועוד מלא הפתעות',
+    'שמעתם נכון, אפילו ברמת הגולן הפריפריאלית אפשר להתאמן כמו שצריך - עם מאמנים מקצועיים, מכשירים מכל הסוגים, מגוון חוגים ועוד מלא הפתעות',
 };
 
 export default function RootLayout({ children }) {
-  useClient(); // Mark the component as a client-side component
-
   return (
     <html lang="he" dir="rtl">
       <body className={rubik.className}>
