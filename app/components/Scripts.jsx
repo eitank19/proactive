6a61dc0f61f57b4ca9064131e354b80bba499e30
eitank19 <142161766+eitank19@users.noreply.github.com>
2024-03-23 21:12:55 +0200

    Update Scripts.jsx

--- app/components/Scripts.jsx
+++ app/components/Scripts.jsx
@@ -3,16 +3,25 @@ import React, { useEffect } from 'react';
 const Scripts = () => {
   useEffect(() => {
     // GTM script code
-    const script = document.createElement('script');
-    script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    const gtmScript = document.createElement('script');
+    gtmScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id=GTM-NQXDVMKB'+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-NQXDVMKB');`;
-    document.head.appendChild(script);
+
+    document.head.appendChild(gtmScript);
+
+    // Enable script code
+    const enableScript = document.createElement('script');
+    enableScript.src = 'https://cdn.enable.co.il/licenses/enable-L2521757f2doiqut-0324-57225/init.js';
+    enableScript.async = true;
+
+    document.body.appendChild(enableScript);
 
     return () => {
-      document.head.removeChild(script);
+      document.head.removeChild(gtmScript);
+      document.body.removeChild(enableScript);
     };
   }, []);
 
